74dfe16d04c213099586ca9f9646f9af6b429cfa
Rashmi-ms <rashmims56@gmail.com>
2021-05-14 13:10:40 +0530

    AmstrongNumber

---
+++ Amstrong number/src/Amstrong.java
@@ -0,0 +1,19 @@
+
+public class Amstrong {
+ public static void main(String[] args) {
+	 int sum=0,r,temp=0;
+	 int n=153;
+	 temp=n;
+	 while(n>0)
+	 {
+		 r=n%10;
+		 sum=sum+(r*r*r);
+		 n=n/10;
+	 }
+	 if(sum==temp)
+		 System.out.println("armstrong number");
+	 else
+		 System.out.println("not armstrong number");
+		 
+ }
+}
