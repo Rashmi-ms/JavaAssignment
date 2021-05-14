051a6e36208b1da5b08c2623c1ccf41ba22410d3
Rashmi-ms <rashmims56@gmail.com>
2021-05-14 16:56:49 +0530

    fail fast and safe

---
+++ CollectionFramework/src/collectionframework/FailSafeDemo1.java
@@ -0,0 +1,25 @@
+package collectionframework;
+
+import java.util.concurrent.ConcurrentHashMap;   
+import java.util.Iterator;   
+public class FailSafeDemo1 {   
+    public static void main(String[] args)   
+    {   
+        // Initializing a ConcurrentHashMap   
+        ConcurrentHashMap<String, Integer> m   
+            = new ConcurrentHashMap<String, Integer>();   
+        m.put("ONE", 1); //Adding values  
+        m.put("SEVEN", 7);   
+        m.put("FIVE", 5);   
+        m.put("EIGHT", 8);   
+        // Getting an iterator using map  
+        Iterator<String> it = m.keySet().iterator();   
+        while (it.hasNext()) {   
+            String key = (String)it.next();   
+            System.out.println(key + " : " + m.get(key));   
+            // This will reflect in iterator.   
+            // This means it has not created separate copy of objects  
+            m.put("NINE", 9);   
+        }   
+    }   
+}  
